9a09284c3e406628320ee010759bc3b4c5ca1dcc
George Zakharov <george_zakharov@mail.ru>
2016-09-13 16:37:55 +0300

    Add FormHandler class. Update main.js: add form check and AJAX-request to the server.

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <!-- Custom main stylesheet -->
     <link rel="stylesheet" href="css/main.css">
+    <!-- Font Awesome stylesheet TODO: Delete if there is no use -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
 </head>
 <body>
     <!-- Header text -->
@@ -18,41 +20,50 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-6 col-sm-offset-3">
+                <!-- Success message -->
+                <div class="col-sm-10 col-sm-offset-2 alert alert-success hidden" role="alert" id="successMessage">
+                    <strong>Внимание!</strong> Ваше сообщение успешно отправлено.
+                </div>
+                <!-- Error message -->
+                <div id="error" class="col-sm-10 col-sm-offset-2" style="color: #ff0000; margin-top: 5px; margin-bottom: 5px;"></div>
                 <!-- Form starts -->
-                <form class="form-horizontal" role="form">
+                <form class="form-horizontal" role="form" id="feedbackForm">
                     <!-- Name input -->
                     <div class="form-group">
-                        <label for="inputName" class="col-sm-2 control-label">Имя</label>
+                        <label for="inputName" class="col-sm-2 control-label">Имя*</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="inputName" placeholder="Фамилия Имя Отчество">
+                            <input type="text" class="form-control" id="inputName" name="inputName" required placeholder="Фамилия Имя Отчество">
+                            <span class="glyphicon form-control-feedback"></span>
                         </div>
                     </div>
                     <!-- Telephone number input -->
                     <div class="form-group">
-                        <label for="inputNumber" class="col-sm-2 control-label">Номер</label>
+                        <label for="inputNumber" class="col-sm-2 control-label">Номер*</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="inputNumber" placeholder="(___) ___-__-__">
+                            <input type="text" class="form-control" id="inputNumber" name="inputNumber" required placeholder="(___)___-__-__">
+                            <span class="glyphicon form-control-feedback"></span>
                         </div>
                     </div>
                     <!-- E-mail input -->
                     <div class="form-group">
                         <label for="inputEmail" class="col-sm-2 control-label">Почта</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="inputEmail" placeholder="you@mail.ru">
+                            <input type="email" class="form-control" id="inputEmail" name="inputEmail" placeholder="you@mail.ru">
+                            <span class="glyphicon form-control-feedback"></span>
                         </div>
                     </div>
                     <!-- Comments input -->
                     <div class="form-group">
-                        <label for="inputNotes" class="col-sm-2 control-label">Комментарии</label>
+                        <label for="inputNotes" class="col-sm-2 control-label">Комментарий</label>
                         <div class="col-sm-10">
-                            <textarea name="notes" class="form-control" id="inputNotes" cols="30" rows="10" placeholder="Ваши комментарии"></textarea>
+                            <textarea name="notes" class="form-control" id="inputNotes" cols="30" rows="10" placeholder="Ваши комментарии (не более 1000 символов)"></textarea>
                         </div>
                     </div>
                     <!-- Comments input -->
                     <div class="form-group">
-                        <label for="isClient" class="col-sm-2 control-label">Клиент</label>
+                        <label for="isClient" class="col-sm-2 control-label">Клиент*</label>
                         <div class="col-sm-10">
-                            <select class="form-control" id="isClient">
+                            <select class="form-control" id="isClient" name="isClient">
                                 <option>Да</option>
                                 <option>Нет</option>
                             </select>
@@ -63,7 +74,8 @@
                         <div class="col-sm-offset-2 col-sm-10">
                             <div class="checkbox">
                                 <label>
-                                    <input type="checkbox" checked> Я согласен с условиями Пользовательского Соглашения
+                                    <input type="checkbox" checked="checked"> Я согласен с условиями Пользовательского Соглашения*
+                                    <span class="glyphicon form-control-feedback"></span>
                                 </label>
                             </div>
                         </div>
@@ -71,8 +83,13 @@
                     <!-- Send feedback -->
                     <div class="form-group">
                         <div class="col-sm-offset-2 col-sm-10">
-                            <button type="submit" class="btn btn-default">Отправить</button>
+                            <button type="submit" class="btn btn-success pull-right">Отправить</button>
                         </div>
+                    </div>
+                    <!-- Conditions -->
+                    <div class="col-sm-offset-2 col-sm-10">
+                        <hr>
+                        <em>* поля, обязательные к заполнению</em>
                     </div>
                 </form>
                 <!-- Form ends -->
@@ -87,6 +104,8 @@
     <script src="js/bootstrap.min.js"></script>
     <!-- Masked Input JQuery plugin -->
     <script src="js/maskedinput.js"></script>
+    <!-- JQuery validation plugin TODO: Delete if no use -->
+    <script src="js/jquery.validate.min.js"></script>
     <!-- Custom JS -->
     <script src="js/main.js"></script>
 </body>
