bbc66e88a9cbe8fd426340d8448dbfc08139ad08
George Zakharov <george_zakharov@mail.ru>
2016-10-01 15:13:36 +0300

    Change language of the form to English

--- core/FormHandler.php
+++ core/FormHandler.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * This file manage input from the client-side via AJAX-request.
+ *
+ * @package: feedback
+ * @version: 1.0
+ * @author: wildwind | George Zakharov
+ */
+
 $name = $_POST['inputName'];
 $number = $_POST['inputNumber'];
 $email = $_POST['inputEmail'];
